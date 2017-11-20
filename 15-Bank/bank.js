09ea6d699dc6e7266c3309ff3fd230442a76df21
Mark Kazanski <markkazanski@gmail.com>
2017-11-19 18:42:41 -0600

    15 Bank working

--- 15-Bank/bank.js
+++ 15-Bank/bank.js
@@ -10,7 +10,7 @@ switch(action){
     case "deposit":
         deposit(amount);
         break;
-    case "":
+    case "withdraw":
         withdraw(amount);
         break;
     case "lotto":
@@ -24,9 +24,80 @@ switch(action){
 function total(){
     fs.readFile("bank.txt", "utf8", function(error, data) {
         var lines = data.split("\r\n");
+        var sum = 0;
+        
+        for(var i=0; i < lines.length; i++){
+            var actions = lines[i].split(" ");
+            
+            if(actions[0] === "deposit"){
+                sum += parseFloat(actions[1]);
+                console.log("deposit: " + actions[0] + " " + actions[1]);
+            }
+            else if( actions[0] === "withdraw" ){
+                sum -= parseFloat(actions[1]);
+                console.log("withdraw: " + actions[0] + " " + actions[1]);
+            }
+        }
+
+        console.log("total: " + sum);
+
     });
 }
 
+function deposit( money ){
+    var data = "deposit " + money + "\r\n";
+    console.log(money);
+    console.log(data);
+
+    fs.appendFile("bank.txt", data, function(err) {
+        // If an error was experienced we say it.
+        if (err) {
+          console.log(err);
+        }
+    
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+          console.log("Content Added!");
+        }    
+    });    
+}
+
+function withdraw( money ){
+    var data = "withdraw " + money + "\r\n";
+    console.log(money);
+    console.log(data);
+
+    fs.appendFile("bank.txt", data, function(err) {
+        // If an error was experienced we say it.
+        if (err) {
+          console.log(err);
+        }
+    
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+          console.log("Content Added!");
+        }    
+    });    
+}
+
+function lotto(){
+    var cost = 1;
+    withdraw(cost);
+
+    var lottoDraw = Math.floor(Math.random() * 5) + 1;
+    var lottoWinner = 3;  
+
+    if(lottoDraw === lottoWinner){
+        deposit(100);
+        console.log("You win 100");
+        total();
+    }else{
+        console.log("No win.")
+        total();
+    }
+}
+
+/*
 fs.appendFile("bank.txt", data, function(err) {
         // If an error was experienced we say it.
         if (err) {
@@ -38,3 +109,4 @@ fs.appendFile("bank.txt", data, function(err) {
           console.log("Content Added!");
         }    
 });
+*/
